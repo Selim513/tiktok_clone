146e7b9a3fcee4bc24496ab8a1ae98e6e9757d58
Ahmed Selim <ahmedshaabanselem@gmail.com>
2025-05-11 22:37:54 +0300

    feat: Integrate Firebase for authentication and video playback; update dependencies and configuration

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,3 +1,71 @@
+// import 'package:better_player_plus/better_player_plus.dart';
+// import 'package:flutter/material.dart';
+
+// class HomeView extends StatelessWidget {
+//   const HomeView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Your Vedios')),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: PageView.builder(
+//               scrollDirection: Axis.vertical,
+//               itemCount: 2,
+//               itemBuilder: (context, index) {
+//                 return VideoScreen();
+//               },
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class VideoScreen extends StatefulWidget {
+//   const VideoScreen({super.key});
+
+//   @override
+//   State<VideoScreen> createState() => _VideoScreenState();
+// }
+
+// class _VideoScreenState extends State<VideoScreen> {
+//   late BetterPlayerController _controller;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     BetterPlayerDataSource dataSource = BetterPlayerDataSource(
+//       BetterPlayerDataSourceType.network,
+//       "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_1MB.mp4",
+//     );
+
+//     _controller = BetterPlayerController(
+//       BetterPlayerConfiguration(
+//         autoPlay: true,
+//         looping: true,
+//         aspectRatio: 9 / 16,
+//         fit: BoxFit.cover,
+//       ),
+//       betterPlayerDataSource: dataSource,
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(body: Center(child: BetterPlayer(controller: _controller)));
+//   }
+// }
 import 'package:better_player_plus/better_player_plus.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +75,22 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Your Vedios')),
+      appBar: AppBar(title: Text('Your Videos')),
       body: Column(
         children: [
+          if (videoUrls.isEmpty)
+            Center(
+              child: Text(
+                'There is no vedio be the first who upload',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
           Expanded(
             child: PageView.builder(
               scrollDirection: Axis.vertical,
-              itemCount: 2,
+              itemCount: videoUrls.length,
               itemBuilder: (context, index) {
-                return VideoScreen();
+                return VideoScreen(videoUrl: videoUrls[index]);
               },
             ),
           ),
@@ -26,7 +101,9 @@ class HomeView extends StatelessWidget {
 }
 
 class VideoScreen extends StatefulWidget {
-  const VideoScreen({super.key});
+  final String videoUrl;
+
+  const VideoScreen({super.key, required this.videoUrl});
 
   @override
   State<VideoScreen> createState() => _VideoScreenState();
@@ -41,7 +118,7 @@ class _VideoScreenState extends State<VideoScreen> {
 
     BetterPlayerDataSource dataSource = BetterPlayerDataSource(
       BetterPlayerDataSourceType.network,
-      "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/720/Big_Buck_Bunny_720_10s_1MB.mp4",
+      widget.videoUrl,
     );
 
     _controller = BetterPlayerController(
@@ -66,3 +143,8 @@ class _VideoScreenState extends State<VideoScreen> {
     return Scaffold(body: Center(child: BetterPlayer(controller: _controller)));
   }
 }
+
+// قائمة روابط الفيديوهات
+final List<String> videoUrls = [
+  "https://cdn.pixabay.com/video/2019/04/06/22634-328940142_small.mp4",
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/core/utils/app_route.dart';
+import 'package:tiktok_clone/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
