f479636cb737ca89193a87d5f0c32b06c73e4952
Ahmed Selim <ahmedshaabanselem@gmail.com>
2025-06-05 18:44:41 +0300

    refactor: Add dotenv package for environment variable management and create .env file for Supabase credentials

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tiktok_clone/core/observer/simple_bloc_observer.dart';
 import 'package:tiktok_clone/core/utils/app_route.dart';
@@ -9,11 +10,13 @@ import 'package:tiktok_clone/features/post_videos/presentation/manger/upload_vid
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   serviceLocatorSetup();
   await Supabase.initialize(
-    url: 'https://sgulxgoevpmygnebscao.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNndWx4Z29ldnBteWduZWJzY2FvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzE1MzQ2OSwiZXhwIjoyMDYyNzI5NDY5fQ.GvgBH6K84Yaz86_TcRkGm1UFTYybADkS_lF5vPrLvm0',
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    //  'https://sgulxgoevpmygnebscao.supabase.co',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNndWx4Z29ldnBteWduZWJzY2FvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NzE1MzQ2OSwiZXhwIjoyMDYyNzI5NDY5fQ.GvgBH6K84Yaz86_TcRkGm1UFTYybADkS_lF5vPrLvm0',
   );
   Bloc.observer = SimpleBlocObserver();
   runApp(const MainApp());
